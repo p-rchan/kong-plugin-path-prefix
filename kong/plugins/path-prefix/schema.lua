f5a5ba473f03c63b134093391982152023119101
farhan.amin <farhan.amin@go-jek.com>
2021-05-07 20:57:08 +0700

    Revert "Update plugin schema for Kong 1.x (#3)"
    This reverts commit 3ba5a6c4f27b1c1e7011528cc05909b2e5b20fcb.

--- kong/plugins/path-prefix/schema.lua
+++ kong/plugins/path-prefix/schema.lua
@@ -1,31 +1,7 @@
-local typedefs = require "kong.db.schema.typedefs"
-
 return {
-  name = "path-prefix",
+  no_consumer = true,
   fields = {
-    {
-      consumer = typedefs.no_consumer
-    },
-    {
-      run_on = typedefs.run_on_first
-    },
-    {
-      protocols = typedefs.protocols_http
-    },
-    {
-      config = {
-        type = "record",
-
-        fields = {
-
-          { path_prefix = {type = "string", required = true} },
-          { escape = {type = "boolean", default = true} },
-
-        }, 
-
-      },
-    },
-  },
-  entity_checks = {
+    path_prefix = {type = "string", required = true, },
+    escape = {type = "bool", default = true}
   },
 }
